56c831c4619a7b7492cedf51e8551304d55b19c0
Adar Deprem <deprem@kth.se>
2025-12-18 16:42:32 +0100

    edited out comments

--- src/persistence/firestoreModel.js
+++ src/persistence/firestoreModel.js
@@ -9,49 +9,6 @@ const app= initializeApp(firebaseConfig);
 const db= getFirestore(app);
 const auth = getAuth(app);
 
-//auth.currentUser   The current user when logged in
-/*
-async function signUp(email, password, buddyName) {
-    try {
-        // 1. Create the Firebase Auth user
-        const userCredential = await createUserWithEmailAndPassword( auth, email, password );
-        const user = userCredential.user;
-        const userDocument = doc(db, "users", user.uid);
-
-        //set buddyname 
-        await setDoc(userDocument, {
-                user: { uid: user.uid, email: email },
-                buddyModel: {
-                    name: buddyName,
-                    buddyType: 0,
-                    clothesHat: 0,
-                    clothesTop: 0,
-                    clothesBottom: 0,
-                    clothesShoes: 0,
-                    stats: { hunger: 100, happiness: 100, energy: 100 },
-                    lastTimeInteracted: new Date(),
-                },
-                uiTheme: [84,92,158],
-        });
-        console.log("User created, saved, and logged in!");
-
-    } catch (error) {
-        console.error("Sign up failed:", error);
-    }
-}
-*/
-
-/*
-async function logIn(email, password) {
-    try {
-        await signInWithEmailAndPassword(auth, email, password);
-        console.log("Logged in successfully!");
-    } catch (error) {
-        console.error("Login error:", error);
-    }
-}
-*/
-
 async function signUp(email, password, buddyName) {
     // 1. Create the Firebase Auth user
     const userCredential = await createUserWithEmailAndPassword( auth, email, password );
