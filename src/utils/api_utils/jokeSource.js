bb4be10a8970ce95735cdf7984638b4cc3fd7bda
Adar Deprem <deprem@kth.se>
2025-12-22 12:17:27 +0100

    fixed joke to not give out to long jokes and deleted unused file

--- src/utils/api_utils/jokeSource.js
+++ src/utils/api_utils/jokeSource.js
@@ -1,17 +1,27 @@
 const BASE_URL = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single"; //These are filters pls, we do not actually want nsfw jokes
+const MAX_LENGTH = 120;
+const MAX_RETRIES = 5;
 
 function gotResponseACB(response) {
     if (!response.ok) throw new Error("No response");
     return response.json();
 }
 
-function returnDataACB(data) {
-    console.log(data.joke);
-    return data.joke || data.results;
+function returnDataACB(data, attempt) {
+    if(data.joke.length <= MAX_LENGTH){
+        console.log(data.joke);
+        return data.joke || data.results;
+    }
+
+    if (attempt >= MAX_RETRIES) {
+        return "Sorry i couldn't coome up with any good jokes, could you ask me again :)";
+    }
+    
+    return generateJoke(attempt + 1); 
 }
 
-export function generateJoke() {
+export function generateJoke(attempt = 0) {
     return fetch(BASE_URL)
         .then(gotResponseACB)
-        .then(returnDataACB);
+        .then(data => returnDataACB(data, attempt));
 }
