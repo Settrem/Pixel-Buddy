c3fa774f4be66fe9a4893e80fa88cd90ca25f849
Razmus <Razmus_n99@hotmail.com>
2025-12-22 16:39:25 +0100

    fixed text

--- src/views/HelpView.jsx
+++ src/views/HelpView.jsx
@@ -10,21 +10,23 @@ function HelpView() {
         </div>
         <div className="text-3xl text-left">
             
-            <div className="border-2 border-black p-2 mb-1">
+            <div className="border-2 border-black p-1 mb-1">
                 Your PixelBuddy has three basic needs: Hunger, Happiness, Energy.
                 Use your PixelBuddy's energy to play mini-games to help nurture them.
                 To feed your buddy you need to play the Apple Catcher mini-game.
                 To keep them happy, you need to play Trivia or let them tell you a joke
                 (PixelBuddies love telling jokes).
-                <img src={statusBar} className=" p-2 mb-1"/>
+                <img src={statusBar} className=" mb-1"/>
             </div>
             
-            <div className="border-2 border-black p-2 mb-1">
-                Playing the acitivies will depelte your energy, and not interacting with your Buddy will
+            <div className="border-2 border-black p-1 mb-1">
+                Playing the activities will deplete your energy, and not interacting with your Buddy will
                 deplete their Hunger and Happiness.
                 By stepping away from the game for a while, the PixelBuddy will recover energy over time.
+                Be careful! If your PixelBuddy has no happiness, it will refuse to change clothes.
+                If their Hunger depletes fully, they will die. 
             </div>
-            <div className="border-2 border-black p-2">
+            <div className="border-2 border-black p-1">
                 Use the Settings icon in the top right corner to change the the theme, and to choose a new name for your PixelBuddy!
             </div>
         </div >
